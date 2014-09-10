da09e67ce81c485a80cba1540fa44d5a3fdf7818
Jeremie Parker <jparker@guimiesoft.com>
2014-09-10 11:11:05 +0200

    fixed versions

--- package.js
+++ package.js
@@ -1,12 +1,14 @@
 Package.describe({
   summary: 'Simple package to use gravatar images',
-  version: '0.0.4',
+  version: '0.1.0',
   git: 'https://github.com/p-j/meteor-gravatar.git'
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.0');
-  api.use('jparker:crypto-md5@3.1.2', ['client', 'server']);
+  api.versionsFrom('METEOR@0.9.1.1');
+
+  api.use('jparker:crypto-md5@0.1.1', ['client', 'server']);
+
   if(api.export)
     api.export('Gravatar');
   api.add_files('gravatar.js', ['client', 'server']);
